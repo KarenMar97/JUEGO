cff38f86f5f0d18061bb97e74592e2757dfa5a00
Karen Mejia <karenmar97@hotmail.com>
2020-06-12 22:32:12 -0500

    inicio codigo

---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+function ingreso() {
+    usuario = document.getElementById("nombre-usuario").value;
+    clave = document.getElementById("clave-usuario").value;
+
+    console.log("usuario: "+usuario+" clave: "+clave);
+}
